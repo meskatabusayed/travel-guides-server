3fe8a3f20ac55a2227d43749469d67af078a86a1
meskatabusayed <meskatabusayed@gmail.com>
2024-10-04 22:21:27 +0530

    modules are added

---
+++ src/app/config/cloud.ts
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dwehbmfy5",
+  api_key: "514945648111989",
+  api_secret: "TqcsTXDjrG97dk4B-WyvWYqq6GQ",
+});
+export default cloudinary;
