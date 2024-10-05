12de9712214351df24922a0414aab9474647444c
meskatabusayed <meskatabusayed@gmail.com>
2024-10-05 12:04:25 +0530

    cloudnary secret added

--- src/app/config/cloud.ts
+++ src/app/config/cloud.ts
@@ -1,8 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dwehbmfy5",
-  api_key: "514945648111989",
-  api_secret: "TqcsTXDjrG97dk4B-WyvWYqq6GQ",
+  cloud_name: process.env.CN_Cloud_name,
+  api_key: process.env.CN_Api_key,
+  api_secret: process.env.CN_Api_secret,
 });
 export default cloudinary;
