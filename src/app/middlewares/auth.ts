74a351e0531802e0581839b4452654109ebb103e
meskatabusayed <meskatabusayed@gmail.com>
2024-10-07 11:02:51 +0530

    auth middleware update

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,3 +1,52 @@
+/* import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from "../config";
+// import catchAsync from "../utils/catchAsync";
+import httpStatus from "http-status";
+import AppError from "../Error/AppError";
+import { CUserRole } from "../Module/User/user.interface";
+import { User } from "../Module/User/user.model";
+import catchAsync from "../utils/catchAsync";
+
+
+export const AuthValidated = (...requierdRole: CUserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+    }
+
+    // Decode the token
+    const decoded = jwt.verify(
+      token.split(' ')[1],
+      config.jwt_access_secret as string
+    ) as JwtPayload;
+ 
+    const { userId } = decoded;
+    
+    // Find the user by ID
+    const isExistsUser = await User.findById(userId);
+    if (!isExistsUser) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+    }
+
+    // Check if the user's role is included in the required roles
+    if (!requierdRole.includes(isExistsUser.role)) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        "You have no access to this route"
+      );
+    }
+
+    // Attach decoded user to request
+    req.user = decoded as JwtPayload;
+    next();
+  });
+}; */
+
+
+//auth related middleware........
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
@@ -37,7 +86,7 @@ export const isAuthenticatedUser = async (
     const auth = await Authentication.findOne({ email: user.email });
     if (!auth) return res.status(404).json({ message: "User does not exist." });
 
-    // console.log("user =======", user);
+   
 
     const payload = user.toObject();
     req.user = { ...payload, role: auth.role };
