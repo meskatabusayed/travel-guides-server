639363a601dfa07adfc636df596b3d9d621b83f1
meskatabusayed <meskatabusayed@gmail.com>
2024-10-07 13:46:17 +0530

    auth done

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,3 +1,41 @@
+/* import httpStatus from "http-status";
+// import catchAsync from "../../utils/catchAsync";
+// import { AuthServices } from "./auth.service";
+import catchAsync from "../../utils/catchAsync";
+import { AuthServices } from "./auth.service";
+
+const loginUserDB = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+  const { user, accessToken } = result;
+  res.status(httpStatus.OK).json({
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User is logged in successfully",
+    token: accessToken,
+    data: user,
+  });
+});
+
+// // refreshToken
+// const refreshTokenDB = catchAsync(async (req, res) => {
+//   const { refreshToken } = req.cookies;
+//   const result = await AuthServices.refreshToken(refreshToken);
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: "Access token is retrieved successfully!",
+//     data: result,
+//   });
+// });
+
+
+export const AuthControllers = {
+  loginUserDB,
+  // refreshTokenDB
+}; */
+
+//
+
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,3 +1,19 @@
+/* import express from "express";
+import { AuthControllers } from "./auth.controllar";
+// import validationRequest from "../../middlewares/validaedRequest";
+// import { AuthValidation } from "./auth.validation";
+import validationRequest from "../../middlewares/validaedRequest";
+import { AuthValidation } from "./auth.validation";
+
+const router = express.Router();
+
+router.post('/auth/login',validationRequest(AuthValidation.loginValidatoinSchema), AuthControllers.loginUserDB);
+
+// router.post('/auth/refresh-token', validationRequest(AuthValidation.refreshTokenValidationSchema), AuthControllers.refreshTokenDB);
+
+export const AuthRoutes = router; */
+
+
 import { Router } from "express";
 import { authorizeRoles, isAuthenticatedUser } from "../../middlewares/auth";
 import {
